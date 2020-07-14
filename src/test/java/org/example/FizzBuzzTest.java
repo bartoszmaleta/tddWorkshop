f2e75b14afc1150500f60cbf3e0e914f0a201377
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-14 16:28:52 +0200

    Add dataProvider

--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -1,15 +1,24 @@
 package org.example;
 
-import org.junit.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class FizzBuzzTest {
 
-    @Test
-    public void should_returnANumber_when_notDivisibleBy3Nor5() {
+    @DataProvider
+    public Object[][] numbersNotDivisibleBy3Nor5() {
+        return new Object[][] {
+                {1},
+                {2},
+                {4},
+        };
+    }
+
+    @Test(dataProvider = "numbersNotDivisibleBy3Nor5")
+    public void should_returnANumber_when_notDivisibleBy3Nor5(int n) {
         // Arrange  // Given
-        int n = 1;
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         // Act      // When
