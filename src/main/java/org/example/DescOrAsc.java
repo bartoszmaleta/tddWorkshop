015cd6be1df73d56d7c65c90b9b9d8fe45285ec4
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-15 01:40:18 +0200

    Add isDescending + test, isNotOrdered() + test, hasDuplicates + test

--- src/main/java/org/example/DescOrAsc.java
+++ src/main/java/org/example/DescOrAsc.java
@@ -6,19 +6,48 @@ import java.util.List;
 
 public class DescOrAsc {
 
-    public boolean isAscending(List<Integer> numbers) {
-        if (numbers.isEmpty() || numbers.size() == 1) {
-            return true;
-        }
+    public boolean isSorted(List<Integer> numbers) {
+        // LONGER Zersion
+//        if (numbers.isEmpty() || numbers.size() == 1) {
+//            return true;
+//        }
+//
+//        return isAscending(numbers) || isDescending(numbers);
 
+
+        // SHORTER version
+        return (numbers.isEmpty() || numbers.size() == 1) || (isAscending(numbers) || isDescending(numbers));
+
+        // Ternary operator version
+//        return (numbers.isEmpty() || numbers.size() == 1) ? true : (isAscending(numbers) || isDescending(numbers));
+    }
+
+    public boolean isAscending(List<Integer> numbers) {
         Iterator<Integer> iter = numbers.iterator();
         Integer current, previous = iter.next();
         while (iter.hasNext()) {
             current = iter.next();
             if (previous.compareTo(current) > 0) {
                 return false;
+            } else if (previous.compareTo(current) == 0) {
+                return false;
             }
             previous = current;
+        }
+        return true;
+    }
+
+    public boolean isDescending(List<Integer> numbers) {
+        Iterator<Integer> iter2 = numbers.iterator();
+        Integer current2, previous2 = iter2.next();
+        while (iter2.hasNext()) {
+            current2 = iter2.next();
+            if (previous2.compareTo(current2) < 0) {
+                return false;
+            } else if (previous2.compareTo(current2) == 0) {
+                return false;
+            }
+            previous2 = current2;
         }
         return true;
     }
--- src/test/java/org/example/DescOrAscTest.java
+++ src/test/java/org/example/DescOrAscTest.java
@@ -15,13 +15,13 @@ public class DescOrAscTest {
     @DataProvider
     public Object[][] ascendingNumbers() {
         return new Object[][] {
-                {new ArrayList<Integer>(
+                {new ArrayList<>(
                         Arrays.asList(2, 4, 7, 8)
                 )},
-                {new ArrayList<Integer>(
+                {new ArrayList<>(
                         Arrays.asList(7, 8, 9, 10)
                 )},
-                {new ArrayList<Integer>(
+                {new ArrayList<>(
                         Arrays.asList(11, 55, 342, 555)
                 )},
         };
@@ -33,10 +33,91 @@ public class DescOrAscTest {
         // Act      // When
         System.out.println(numbers);
 
-        boolean result = descOrAsc.isAscending(numbers);
+        boolean result = descOrAsc.isSorted(numbers);
         // Assert   // Then
 
         assertTrue(result);
+    }
+
+    @DataProvider
+    public Object[][] descendingNumbers() {
+        return new Object[][] {
+                {new ArrayList<>(
+                        Arrays.asList(10, 5, 2)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(104, 54, 22)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(1012, 543, 22)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(101, 52, 2)
+                )},
+        };
+    }
+
+    @Test(dataProvider = "descendingNumbers")
+    public void should_returnTrue_when_numbersAreInDescendingOrder(List<Integer> numbers) {
+        // Act
+        boolean result = descOrAsc.isSorted(numbers);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @DataProvider
+    public Object[][] notOrderedNumbers() {
+        return new Object[][] {
+                {new ArrayList<>(
+                        Arrays.asList(10, 5, 22)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(104, 542, 22)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(1012, 5423, 22)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(10, 52, 2)
+                )},
+        };
+    }
+
+    @Test(dataProvider =  "notOrderedNumbers")
+    public void should_returnFalse_when_numbersAreNotOrdered(List<Integer> numbers) {
+        // Act
+        boolean result = descOrAsc.isSorted(numbers);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @DataProvider
+    public Object[][] numbersWithDuplicates() {
+        return new Object[][] {
+                {new ArrayList<>(
+                        Arrays.asList(10, 5, 10)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(104, 104, 22)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(1012, 22, 22)
+                )},
+                {new ArrayList<>(
+                        Arrays.asList(10, 2, 10)
+                )},
+        };
+    }
+
+    @Test(dataProvider =  "numbersWithDuplicates")
+    public void should_returnFalse_when_numbersHaveDuplicates(List<Integer> numbers) {
+        // Act
+        boolean result = descOrAsc.isSorted(numbers);
+
+        // Assert
+        assertFalse(result);
     }
 
 }
