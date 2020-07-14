9b063498f9172259041b91edd6a2d926dc7c780d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-14 16:34:20 +0200

    Add another test

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class FizzBuzz {
     public String accept(int n) {
-//        return String.valueOf(n);
-        return "1";
+        if (n == 3) return "fizz";
+
+        return String.valueOf(n);
     }
 }
--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.testng.ITest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -26,6 +27,19 @@ public class FizzBuzzTest {
 
         // Assert   // Then
         assertEquals(result, String.valueOf(n));
+    }
+
+    @Test
+    public void should_returnFizz_when_numberIsDivisibleBy3() {
+        // Arrange
+        int n = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // Act
+        String result = fizzBuzz.accept(n);
+
+        // Assert
+        assertEquals(result, "fizz");
     }
 
 }
