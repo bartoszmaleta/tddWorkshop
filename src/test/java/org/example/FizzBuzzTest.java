32b636475562cb829f0035a6eb7fe88d3caf96e6
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-14 16:07:33 +0200

    Add junit5 and testNG dependencies

---
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.testng.Assert.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void should_returnANumber_when_notDivisibleBy3Nor5() {
+
+    }
+
+}
