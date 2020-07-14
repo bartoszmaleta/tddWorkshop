ba667397d7576f1d4f825afea3defc6887cd681b
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-14 16:16:55 +0200

    Add 3 A to test

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class FizzBuzz {
+    public String accept(int n) {
+//        return String.valueOf(n);
+        return "1";
+    }
 }
--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -8,7 +8,15 @@ public class FizzBuzzTest {
 
     @Test
     public void should_returnANumber_when_notDivisibleBy3Nor5() {
+        // Arrange  // Given
+        int n = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
 
+        // Act      // When
+        String result = fizzBuzz.accept(n);
+
+        // Assert   // Then
+        assertEquals(result, String.valueOf(n));
     }
 
 }
