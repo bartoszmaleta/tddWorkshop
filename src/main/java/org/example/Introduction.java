d37428c44831e3230e129aaaee52d185d181c9b8
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-14 16:46:51 +0200

    Add exercise command to do

--- src/main/java/org/example/Introduction.java
+++ src/main/java/org/example/Introduction.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class Introduction {
+
+    // przyjmuję listę integerów
+    // zwraca boolean
+    // sprawdza czy integery są w kolejności (malejącej lub rosnącej)
+
+    // accept list of integers
+    // return boolean
+    // checks if integers are in order (desc or asc)
+    // desc && asc ===> true
+    // different ===> false
+    // duplicates ===> false
 }
