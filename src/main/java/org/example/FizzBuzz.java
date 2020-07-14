03c36655475d3b4e9c70dcd1796102954b304e12
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-14 16:42:36 +0200

    Add logic that meets test requirements

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class FizzBuzz {
     public String accept(int n) {
-        if (n == 3) return "fizz";
+        if (n % 3 == 0) return "fizz";
 
         return String.valueOf(n);
     }
